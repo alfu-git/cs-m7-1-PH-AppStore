8adca7b4970597040cdf5f9bfcfb36ee0e6762de
alfu-git <mdalfaz.dev@gmail.com>
2026-04-09 01:29:24 +0600

    show all apps on apps page

---
+++ src/Components/AppsPage/AllAppsContainer/AllAppsContainer.jsx
@@ -0,0 +1,80 @@
+import React, { useEffect, useState } from "react";
+import { HashLoader } from "react-spinners";
+import TrendingAppCard from "../../HomePage/TrendingApps/TrendingAppCard/TrendingAppCard";
+
+const AllAppsContainer = () => {
+  const [allApps, setAllApps] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchAllAppsData = async () => {
+      const res = await fetch("/data.json");
+      const data = await res.json();
+
+      setTimeout(() => {
+        setAllApps(data);
+        setLoading(false);
+      }, 2000);
+    };
+
+    fetchAllAppsData();
+  }, []);
+
+  return (
+    <section className="mb-20 container mx-auto px-5">
+      <div>
+        <div>
+          <div className="mb-4 flex flex-col sm:flex-row gap-y-4 justify-between sm:items-center">
+            <div>
+              <p className="flex gap-1 items-center text-[#001931] text-2xl font-semibold">
+                <span>(15)</span>
+                <span>Apps Found</span>
+              </p>
+            </div>
+
+            <div>
+              <label className="input">
+                <svg
+                  className="h-[1em] opacity-50"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                >
+                  <g
+                    strokeLinejoin="round"
+                    strokeLinecap="round"
+                    strokeWidth="2.5"
+                    fill="none"
+                    stroke="currentColor"
+                  >
+                    <circle cx="11" cy="11" r="8"></circle>
+                    <path d="m21 21-4.3-4.3"></path>
+                  </g>
+                </svg>
+
+                <input type="search" required placeholder="Search" />
+              </label>
+            </div>
+          </div>
+
+          <div>
+            {loading ? (
+              <div className="mt-10 flex justify-center">
+                <HashLoader color="#632EE3" />
+              </div>
+            ) : (
+              <div>
+                <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+                  {allApps.map((app) => (
+                    <TrendingAppCard key={app.id} app={app} />
+                  ))}
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AllAppsContainer;
--- src/Pages/AppsPage/AppsPage.jsx
+++ src/Pages/AppsPage/AppsPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import AppsPageTitle from '../../Components/AppsPage/AppsPageTitle/AppsPageTitle';
+import AllAppsContainer from '../../Components/AppsPage/AllAppsContainer/AllAppsContainer';
 
 const AppsPage = () => {
   return (
     <>
       <AppsPageTitle />
+      <AllAppsContainer />
     </>
   );
 };
