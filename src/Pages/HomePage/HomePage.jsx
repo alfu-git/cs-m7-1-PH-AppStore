a28617ed8437c7e7c6ef7cf575661d35865a39a3
alfu-git <mdalfaz.dev@gmail.com>
2026-04-09 00:38:04 +0600

    show apps on home page

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from "../../Components/HomePage/Banner/Banner";
 import Stats from '../../Components/HomePage/Stats/Stats';
+import TrendingApps from '../../Components/HomePage/TrendingApps/TrendingApps';
 
 const HomePage = () => {
   return (
     <>
       <Banner />
       <Stats />
+      <TrendingApps />
     </>
   );
 };
